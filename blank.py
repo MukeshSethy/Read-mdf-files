d80326aefc24c18edd27fc3b2f8546b5412866d6
MukeshSethy <43363581+MukeshSethy@users.noreply.github.com>
2018-09-18 13:12:45 +0530

    Code to delete all blank rows

--- blank.py
+++ blank.py
@@ -1,8 +1,8 @@
-import csv
-with open('Dauerzeitschrieb_20171218_053831_Test.csv', 'r') as inp, open('Without_space_data.csv', 'w', newline='') as out
-    writer = csv.writer(out)
+import csv
+with open('Dauerzeitschrieb_20171218_053831.csv', 'r') as inp, open('Without_space_data.csv', 'w', newline='') as out:
+    writer = csv.writer(out)
     it=0
-    for row in csv.reader(inp):
+    for row in csv.reader(inp):
         it=it+1
-        if it % 2 != 0:
-            writer.writerow(row) 
+        if it % 2 != 0:
+            writer.writerow(row)
