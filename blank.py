a687fb2770dcfed2a0a8f716ffc8479b3d88acf3
MukeshSethy <43363581+MukeshSethy@users.noreply.github.com>
2018-09-18 13:10:43 +0530

    Code to delete all blank rows from an excel file

---
+++ blank.py
@@ -0,0 +1,8 @@
+import csv
+with open('Dauerzeitschrieb_20171218_053831_Test.csv', 'r') as inp, open('Without_space_data.csv', 'w', newline='') as out
+    writer = csv.writer(out)
+    it=0
+    for row in csv.reader(inp):
+        it=it+1
+        if it % 2 != 0:
+            writer.writerow(row) 
